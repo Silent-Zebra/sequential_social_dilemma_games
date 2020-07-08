134aea5421f1cf9e28b592e2525bcd704732c679
Stephen Zhao <szstephenzhao88@gmail.com>
2020-07-08 14:49:55 -0400

    debug

--- train_agents.py
+++ train_agents.py
@@ -98,8 +98,13 @@ def on_episode_end(info):
 
     # Need env.agents
     print("Extrinsic Rewards:")
-    for agent in info["env"]["envs"][0].agents.values():
-        print(agent.extrinsic_reward_record)
+    print(info["env"]["envs"])
+    print(info["env"]["envs"][0])
+    print(info["env"]["envs"][0].agents)
+    print(info["env"]["envs"][0].agents.values())
+
+    # for agent in info["env"]["envs"][0].agents.values():
+    #     print(agent.extrinsic_reward_record)
 
     sys.stdout.flush()
 
