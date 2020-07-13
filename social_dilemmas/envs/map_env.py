1e91c8b1c901f6f8768bb1863876c3320aa817d4
Stephen Zhao <szstephenzhao88@gmail.com>
2020-07-13 00:29:46 -0400

    more debug

--- social_dilemmas/envs/map_env.py
+++ social_dilemmas/envs/map_env.py
@@ -202,10 +202,18 @@ class MapEnv(MultiAgentEnv):
                 lambdgamma = 0.975 # smoothing hyperparam here TODO make arg/setting
                 agent.smoothed_extrinsic_reward = lambdgamma * agent.smoothed_extrinsic_reward + rew
                 smoothed_rew_list.append(agent.smoothed_extrinsic_reward)
+
+        print("Smooth rew list")
+        print(smoothed_rew_list)
+        import sys
+        sys.stdout.flush()
+
         if intrinsic_reward:
             for agent in self.agents.values():
                 agent.smoothed_rew_arr = np.array(smoothed_rew_list)
-                observations[agent.agent_id] = (observations[agent.agent_id], agent.smoothed_rew_arr)
+                # observations[agent.agent_id] = (observations[agent.agent_id], agent.smoothed_rew_arr)
+
+
             # Start with a constant parameter, later we'll have it as a
             # property of each agent, such as agent.svo = 0.9 or something
             # And then have a more sophisticated calc
@@ -282,12 +290,19 @@ class MapEnv(MultiAgentEnv):
 
         observations = {}
         n_agents = len(self.agents.values())
+
+        print("nagents")
+        print(n_agents)
+        import sys
+        sys.stdout.flush()
+
         for agent in self.agents.values():
             agent.grid = map_with_agents
             # agent.grid = util.return_view(map_with_agents, agent.pos,
             #                               agent.row_size, agent.col_size)
             rgb_arr = self.map_to_colors(agent.get_state(), self.color_map)
-            observations[agent.agent_id] = (rgb_arr, np.zeros(n_agents))
+            observations[agent.agent_id] = rgb_arr
+            # observations[agent.agent_id] = (rgb_arr, np.zeros(n_agents))
             agent.extrinsic_reward_sum = 0
             agent.smoothed_extrinsic_reward = 0
         return observations
