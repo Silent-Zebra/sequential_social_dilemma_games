9279248fc8504423ebd53501547564fb9c5473c4
Stephen Zhao <szstephenzhao88@gmail.com>
2020-07-18 01:16:08 -0400

    revert map

--- social_dilemmas/constants.py
+++ social_dilemmas/constants.py
@@ -4,6 +4,7 @@
 # 'A' means apply spawn point
 # '' is empty space
 
+# Big
 # HARVEST_MAP = [
 #     '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',
 #     '@   P   P  P  AA P  P P AA A P  P  A P  P A  AA  A P @',
@@ -29,33 +30,35 @@
 #     '@   P   P  P  A  P  AA   P  A  P A     P   P P     P @',
 #     '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@']
 
-HARVEST_MAP = [
-    '@@@@@@@@@@',
-    '@ P AA P @',
-    '@P AAAA  @',
-    '@ AAA A P@',
-    '@P AAA P @',
-    '@  AA    @',
-    '@ P  P P @',
-    '@@@@@@@@@@']
-
+# Tiny
 # HARVEST_MAP = [
-#     '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',
-#     '@ P   P      A    P AAAAA    P  A P  @',
-#     '@  P     A P AA    P    AAA    A  A  @',
-#     '@     A AAA  AAA    A    A AA AAAA   @',
-#     '@ A  AAA A    A  A AAA  A  A   A A   @',
-#     '@AAA  A A    A  AAA A  AAA        A P@',
-#     '@ A A  AAA  AAA  A A    A AA   AA AA @',
-#     '@  A A  AAA    A A  AAA    AAA  A    @',
-#     '@   AAA  A      AAA  A    AAAA       @',
-#     '@ P  A       A  A AAA    A  A      P @',
-#     '@A  AAA  A  A  AAA A    AAAA     P   @',
-#     '@    A A   AAA  A A      A AA   A  P @',
-#     '@     AAA   A A  AAA      AA   AAA P @',
-#     '@ A    A     AAA  A  P          A    @',
-#     '@       P     A         P  P P     P @',
-#     '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@']
+#     '@@@@@@@@@@',
+#     '@ P AA P @',
+#     '@P AAAA  @',
+#     '@ AAA A P@',
+#     '@P AAA P @',
+#     '@  AA    @',
+#     '@ P  P P @',
+#     '@@@@@@@@@@']
+
+# Default
+HARVEST_MAP = [
+    '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@',
+    '@ P   P      A    P AAAAA    P  A P  @',
+    '@  P     A P AA    P    AAA    A  A  @',
+    '@     A AAA  AAA    A    A AA AAAA   @',
+    '@ A  AAA A    A  A AAA  A  A   A A   @',
+    '@AAA  A A    A  AAA A  AAA        A P@',
+    '@ A A  AAA  AAA  A A    A AA   AA AA @',
+    '@  A A  AAA    A A  AAA    AAA  A    @',
+    '@   AAA  A      AAA  A    AAAA       @',
+    '@ P  A       A  A AAA    A  A      P @',
+    '@A  AAA  A  A  AAA A    AAAA     P   @',
+    '@    A A   AAA  A A      A AA   A  P @',
+    '@     AAA   A A  AAA      AA   AAA P @',
+    '@ A    A     AAA  A  P          A    @',
+    '@       P     A         P  P P     P @',
+    '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@']
 
 CLEANUP_MAP = [
     '@@@@@@@@@@@@@@@@@@',
