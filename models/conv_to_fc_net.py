ae63934b32230f09515825a9836f2eba31fd6c96
Stephen Zhao <szstephenzhao88@gmail.com>
2020-08-12 18:29:14 -0400

    update

--- models/conv_to_fc_net.py
+++ models/conv_to_fc_net.py
@@ -8,9 +8,22 @@
 
 import tensorflow as tf
 
-from ray.rllib.models.misc import normc_initializer, flatten
 from ray.rllib.models.model import Model
 import tensorflow.contrib.slim as slim
+import numpy as np
+
+
+def normc_initializer(std=1.0):
+    def _initializer(shape, dtype=None, partition_info=None):
+        out = np.random.randn(*shape).astype(np.float32)
+        out *= std / np.sqrt(np.square(out).sum(axis=0, keepdims=True))
+        return tf.constant(out)
+
+    return _initializer
+
+def flatten(x):
+    return tf.reshape(x, [-1, np.prod(x.get_shape().as_list()[1:])])
+
 
 
 class ConvToFCNet(Model):
